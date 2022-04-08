73598ccbcc1500d6f9c8f62c43d726597704aec5
Wyatt <95558468+KCNOSU29K3@users.noreply.github.com>
2022-04-08 10:52:52 -0600

    Updated __init__.py to reflect changes in ciphers.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .ciphers import Ciphers, Cipher_Constructor, hash_, random_choice, compare_hashes, token_generation, gen_random_password, sec_delete, delete
+from .ciphers import hash_, random_choice, compare_hashes, token_generate, generate_password, sec_delete, delete, XOR, is_prime_number, get_prime_number, get_GCD, englishToKey, keyToEnglish, Ciphers, Cipher_Constructor, init_diagonostic
 from .log import Log
